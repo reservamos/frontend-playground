f191700afb0dac50f91c414cb5c5cc5c635dbd2c
Juan Miguel Rubio <jmrubio42@gmail.com>
2019-03-08 10:47:24 -0600

    Bourbon

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 const withSass = require('@zeit/next-sass');
+const bourbon = require('bourbon');
 
 module.exports = withSass({
   cssModules: true,
+  sassLoaderOptions: {
+    includePaths: [bourbon.includePaths],
+  },
 });
