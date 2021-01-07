8d82164d6a969a921f138b36a00203d6412b0cb3
brendaorozcoreservamos <75743401+brendaorozcoreservamos@users.noreply.github.com>
2021-01-07 16:47:58 -0600

    button change to ahref , class names change

--- pages/error.js
+++ pages/error.js
@@ -10,23 +10,19 @@ function Home () {
             <img src="https://assets-fl.reservamos.mx/assets/logo-b09feb00a641615eb083fedea266d68ed48e1e46687e198437b7a1aab251860d.svg" alt="reservamos" className="logo" />
           </a>
         </div>
-        <div className="illust">
+        <div className="illustration">
           <img src="https://i.ibb.co/x8HnLbT/illust.png" alt="illustration" />
         </div>
-        <div className="text-one">
-          <h1>Disculpa, la página que estás buscando no existe.</h1>
-          <p>Puede ser alguno de los siguientes motivos:</p>
-          <ul>
+        <div className="description-one">
+          <h1 className="description-title">Disculpa, la página que estás buscando no existe.</h1>
+          <p className="description-paragraph">Puede ser alguno de los siguientes motivos:</p>
+          <ul className="description-list">
             <li>La página ha sido removida del sitio.</li>
             <li>La dirección URL se escribió de manera incorrecta.</li>
             <li>No cuentas con los permisos correctos para acceder a esta sección.</li>
           </ul>
-          <div className="button-one">
-            <a href="https://www.reservamos.mx/" target="_blank" rel="noopener noreferrer">Regresar al inicio</a>
-          </div>
-          <div className="button-two">
-            <a href="https://paquetes.reservamos.mx/" target="_blank" rel="noopener noreferrer">Conoce nuestros paquetes</a>
-          </div>
+          <a className="button-primary" href="https://www.reservamos.mx/" target="_blank" rel="noopener noreferrer">Regresar al inicio</a>
+          <a className="description-link" href="https://paquetes.reservamos.mx/" target="_blank" rel="noopener noreferrer">Conoce nuestros paquetes</a>
         </div>
       </div>
 
@@ -35,7 +31,7 @@ function Home () {
         <div className="links">
           <div className="rutasMX">
             <h3>Explora nuestras rutas en Mexico</h3>
-            <div className="tags-item">
+            <div className="button-secondary">
               <a className="tags-link" title="Destinos y rutas de autobús en México" href="#mexico">México</a>
               <a className="tags-link" title="Destinos y rutas de autobús en Puebla" href="#puebla">Puebla</a>
               <a className="tags-link" title="Destinos y rutas de autobús en Distrito Federal" href="#distrito-federal">Distrito Federal</a>
@@ -46,7 +42,7 @@ function Home () {
           </div>
           <div className="links">
             <h3>Explora nuestras rutas en EUA</h3>
-            <div className="tags-item-two">
+            <div className="button-secondary-two">
               <a className="tags-link" title="Destinos y rutas de autobús en Texas" href="#texas">Texas</a>
               <a className="tags-link" title="Destinos y rutas de autobús en California" href="#california">California</a>
               <a className="tags-link" title="Destinos y rutas de autobús en North Carolina" href="#north-carolina">North Carolina</a>
